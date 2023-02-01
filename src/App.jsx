95b3b97eef54cb3409c9f4ab94e0c2bcbee72dd7
Jahagiresh <jahangiresh@code.edu.az>
2023-02-01 00:41:40 -0800

    settings,login,sidebarScroll

--- src/App.jsx
+++ src/App.jsx
@@ -11,37 +11,38 @@ import Products from "./pages/Products";
 import Users from "./pages/Users";
 import ProductDetails from "./pages/ProductDetails";
 import Genders from "./pages/Genders";
-import { Provider } from 'react-redux';
-import { store } from '../src/features/store/index';
+import { Provider } from "react-redux";
+import { store } from "../src/features/store/index";
 import CreateGender from "./pages/CreateGender";
 import Categories from "./pages/Categories";
 import CreateCategory from "./pages/CreateCategory";
-import CreateProducts from './pages/CreateProducts'
+import CreateProducts from "./pages/CreateProducts";
+import Settings from "./pages/Settings";
+import Login from "./pages/Login";
 function App() {
   return (
     <Provider store={store}>
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="orders" element={<Orders />} />
             <Route path="products" element={<Products />} />
+            <Route path="login" element={<Login />} />
             <Route path="customers" element={<Users />} />
-            <Route path="settings" element={<Blank />} />
+            <Route path="settings" element={<Settings />} />
             <Route path="stats" element={<Blank />} />
             <Route path="homepage" element={<PageSettings />} />
             <Route path="productdetails" element={<ProductDetails />} />
             <Route path="genders" element={<Genders />} />
-            <Route path='creategender' element={<CreateGender />} />
-            <Route path='categories' element={<Categories />} />
-            <Route path='createcategory' element={<CreateCategory />} />
-            <Route path='CreateProduct' element={<CreateProducts />} />
+            <Route path="creategender" element={<CreateGender />} />
+            <Route path="categories" element={<Categories />} />
+            <Route path="createcategory" element={<CreateCategory />} />
+            <Route path="CreateProduct" element={<CreateProducts />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </Provider>
-
   );
 }
 
